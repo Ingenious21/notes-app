936d35e138147fc9c7bf0aaaa123f378c6b053b8
Nehemiah Kemayah <81781111+Ingenious21@users.noreply.github.com>
2024-06-05 19:24:55 +0000

    Notes App

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ document.getElementById('note-form').addEventListener('submit', function(event) 
         saveNote();
         displayNotes();
         document.getElementById('note-form').reset();
+        window.location.href = 'index.html';  // Redirect to home screen
     }
 });
 
